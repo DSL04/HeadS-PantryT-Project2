c7e1d0e250298f4a8097b4803a243ed48fde225f
DSL04 <souit02@outlook.com>
2024-08-04 21:32:57 -0400

    - fixed security issue
    - deleted old db and created a new one for new firebaseConfigs since old was accidently pushed
    - added .env for env variables

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import {getFirestore} from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAi5FgV5Q_brBszutM9fOwFgHCvy5tC7kM",
-  authDomain: "inventory-management-ff538.firebaseapp.com",
-  projectId: "inventory-management-ff538",
-  storageBucket: "inventory-management-ff538.appspot.com",
-  messagingSenderId: "61256870737",
-  appId: "1:61256870737:web:cfa3d39b924370f8be4170",
-  measurementId: "G-M4NPPGCXT3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
